87a6415135598b80e919386a1744e0d7794a753c
GosseFlorian <floriangosse91@gmail.com>
2026-07-02 10:01:09 +0200

    déplacement dans le dossier controller pour plus de clarté

--- src/main/java/fr/ada/java_blog/controller/PingController.java
+++ src/main/java/fr/ada/java_blog/controller/PingController.java
@@ -1,4 +1,4 @@
-package fr.ada.java_blog;
+package fr.ada.java_blog.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
